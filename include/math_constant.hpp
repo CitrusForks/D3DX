75279918f78a340d328e3cc6c00f787974036b8f
banterle <frabante@gmail.com>
2014-11-17 22:12:14 +0100

    improving documentation...

--- include/math_constant.hpp
+++ include/math_constant.hpp
@@ -30,16 +30,16 @@ const float C_EPSILON           = 1e-6f;
 //Square root of 2
 const float C_SQRT_2            = 1.4142135623730950488016887242097f;
 
-//PI/2
+//PI / 2
 const float C_PI_05             = 1.5707963267948966192313216916398f;
 
 //PI
 const float C_PI                = 3.1415926535897932384626433832795f;
 
-//(PI *2)
+//(PI * 2)
 const float C_PI_2              = 6.283185307179586476925286766559f;
 
-// 1 / (PI *2)
+// 1 / (PI * 2)
 const float C_INV_PI_2          = 0.159154943091895335768883763f;
 
 //PI times 4
@@ -48,10 +48,10 @@ const float C_PI_4              = 12.566370614359172953850573533118f;
 //One over PI times 4
 const float C_INV_PI_4          = 0.07957747154594766788444188168626f;
 
-//PI*PI*2
+//PI * PI * 2
 const float C_PI_2_2            = 19.739208802178717237668981999752f;
 
-//PI*4/3 --> Sphere volume constant
+//PI * 4 / 3 --> Sphere volume constant
 const float C_SPHERE_VOLUME     = 4.188790204786390984616857844373f;
 
 // 1/PI
